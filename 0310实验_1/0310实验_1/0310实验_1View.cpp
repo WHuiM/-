ba44aa340af1aef5917b3aec9be9cd0fa963792a
e <2560109012@qq.com>
2020-03-29 23:30:54 +0800

    鼠标点击获得客户区消息后，鼠标左键点击画出客户区最大的圆，保证窗口重绘后圆还能显示出来

---
+++ 0310实验_1/0310实验_1/0310实验_1View.cpp
@@ -0,0 +1,99 @@
+
+// 0310实验_1View.cpp : CMy0310实验_1View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "0310实验_1.h"
+#endif
+
+#include "0310实验_1Doc.h"
+#include "0310实验_1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy0310实验_1View
+
+IMPLEMENT_DYNCREATE(CMy0310实验_1View, CView)
+
+BEGIN_MESSAGE_MAP(CMy0310实验_1View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMy0310实验_1View 构造/析构
+
+CMy0310实验_1View::CMy0310实验_1View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy0310实验_1View::~CMy0310实验_1View()
+{
+}
+
+BOOL CMy0310实验_1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy0310实验_1View 绘制
+
+void CMy0310实验_1View::OnDraw(CDC* /*pDC*/)
+{
+	CMy0310实验_1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy0310实验_1View 诊断
+
+#ifdef _DEBUG
+void CMy0310实验_1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy0310实验_1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy0310实验_1Doc* CMy0310实验_1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy0310实验_1Doc)));
+	return (CMy0310实验_1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy0310实验_1View 消息处理程序
+
+
+void CMy0310实验_1View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CClientDC dc(this);
+	CRect cr;
+	int r;
+	this->GetClientRect(&cr);
+	if (cr.bottom - cr.top > cr.right - cr.left)
+		r = (cr.right - cr.left) / 2;
+	else
+		r = (cr.bottom + cr.top )/ 2;
+	dc.Ellipse((cr.right - cr.left) / 2 - r, (cr.bottom - cr.top) / 2 - r, (cr.right - cr.left) / 2 + r, (cr.bottom - cr.top) / 2 + r);
+	
+	CView::OnLButtonDown(nFlags, point);
+}
